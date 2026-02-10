e21939b8f083f0c9fce36ce57e22f432096e20ac
Nathan Niesen <nniesen.mn@gmail.com>
2026-02-10 16:09:21 -0600

    Add reproducer and workaround tests to MessageSpec.groovy

--- src/test/groovy/org/MessageSpec.groovy
+++ src/test/groovy/org/MessageSpec.groovy
@@ -4,14 +4,28 @@ import spock.lang.Specification
 
 class MessageSpec extends Specification {
 
-    def "should create a simple message"() {
-        given: "a message text"
-        def messageText = "Hello, World!"
+    def testMessageSource(String languageCode, String startsWith) {
+        expect: 'messageSource returns a value for the given language code and key'
+        assert startsWith != null // irrelevant for reproducing regression
 
-        when: "we process the message"
-        def result = messageText.toUpperCase()
+        where:
+        languageCode | startsWith
+        'en'         | 'As of '
+        'fr'         | 'À compter du '
+        'zh-TW'      | '截至 '
+    }
 
-        then: "the message should be converted to uppercase"
-        result == "HELLO, WORLD!"
+    
+    // Workaround for Gradle 9.3.0 and 9.3.1
+    // Don't add startsWith to the test names. Java Strings are inherently UTF-16 and some chars cause issues with testcase names if not encoded to UTF-8.
+    def "testWorkaroundMessageSource [languageCode: #languageCode, ##iterationIndex]"(String languageCode, String startsWith) {
+        expect: 'messageSource returns a value for the given language code and key'
+        assert startsWith != null // irrelevant for reproducing regression
+
+        where:
+        languageCode | startsWith
+        'en'         | 'As of '
+        'fr'         | 'À compter du '
+        'zh-TW'      | '截至 '
     }
 }
